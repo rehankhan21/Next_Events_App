262488beda70aec7edd11b365715aea55b57432c
rehankhan21 <narutop224@gmail.com>
2023-04-06 00:54:36 -0400

    added Events List

---
+++ components/events/EventItem.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+function EventItem(props) {
+  const { title, image, date, location, id } = props;
+
+  const humanReadAbleDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedAdress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li>
+      <img src={"/" + image} alt={title} />
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            <time>{humanReadAbleDate}</time>
+          </div>
+          <div>
+            <address>{formattedAdress}</address>
+          </div>
+        </div>
+        <div>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default EventItem;
---
+++ components/events/EventList.js
@@ -0,0 +1,20 @@
+import EventItem from "./EventItem";
+
+function EventList(props) {
+  const { items } = props;
+  return (
+    <ul>
+      {items.map((event) => (
+        <EventItem
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          date={event.date}
+          image={event.image}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default EventList;
