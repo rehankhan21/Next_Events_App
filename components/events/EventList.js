3e117c3b4f3952a3a702d061e99e98406abca120
rehankhan21 <narutop224@gmail.com>
2023-04-06 01:05:03 -0400

    added css module classes for events

--- components/events/EventList.js
+++ components/events/EventList.js
@@ -1,9 +1,10 @@
 import EventItem from "./EventItem";
+import classes from "./EventList.module.css";
 
 function EventList(props) {
   const { items } = props;
   return (
-    <ul>
+    <ul className={classes.list}>
       {items.map((event) => (
         <EventItem
           id={event.id}
