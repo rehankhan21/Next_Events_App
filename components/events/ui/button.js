a311c6281ddbf45b99e93d1fc9d8a4af1823d87b
rehankhan21 <narutop224@gmail.com>
2023-04-07 01:52:07 -0400

    added button to event item and added icons

---
+++ components/events/ui/button.js
@@ -0,0 +1,13 @@
+const { default: Link } = require("next/link");
+
+import classes from "./button.module.css";
+
+function Button(props) {
+  return (
+    <Link href={props.link} className={classes.btn}>
+      {props.children}
+    </Link>
+  );
+}
+
+export default Button;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import { getFeaturedEvents } from "../../dummy-data";
-import EventList from "../../components/events/EventList";
+import EventList from "../../components/events/events/EventList";
 
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
