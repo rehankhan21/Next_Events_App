a6331765c2ea0ab6c72ba678187d3ce625a359ca
rehankhan21 <narutop224@gmail.com>
2023-04-07 02:01:28 -0400

    added event details for specfic eventid paths

--- src/pages/events/[eventId].js
+++ src/pages/events/[eventId].js
@@ -1,8 +1,34 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventContent from "../../../components/events/event-detail/event-content";
+import EventLogistics from "../../../components/events/event-detail/event-logistics";
+import EventSummary from "../../../components/events/event-detail/event-summary";
+import { getEventById } from "../../../dummy-data";
+
 function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No Event Found!</p>;
+  }
+
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    //Use react fragment to display compoenets side by side
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
 
