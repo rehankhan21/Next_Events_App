404cd6fb5460db9a99a4f3e40bbe4bac13eb21a1
rehankhan21 <narutop224@gmail.com>
2023-04-09 21:14:42 -0400

    added filtered events logic

--- src/pages/events/[...slug].js
+++ src/pages/events/[...slug].js
@@ -1,4 +1,40 @@
-function FilteredEventsPage(params) {
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../../dummy-data";
+
+function FilteredEventsPage() {
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  // when the componenet renders for the first time the url data is null
+  // it automatically renders again but with the data.
+  if (!filterData) {
+    return <p className="'center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  // + in front of the string turns it into a int
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid Filter. Please adjust the values</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events for chosen filter</p>;
+  }
+
   return (
     <div>
       <h1>Filtered Events</h1>
--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -2,13 +2,21 @@ import { getAllEvents } from "../../../dummy-data";
 import EventList from "../../../components/events/events/EventList";
 import EventsSearch from "../../../components/events/events/EventsSearch";
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 
 function EventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}a`;
+
+    router.push(fullPath);
+  }
 
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   );
